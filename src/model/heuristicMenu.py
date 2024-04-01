67be30636e2febd49430c503e73671c7782372e0
dsferreira <unidsferreira2003@gmail.com>
2024-04-01 20:50:30 +0100

    Documentation for the heuristics menu

--- src/model/heuristicMenu.py
+++ src/model/heuristicMenu.py
@@ -5,14 +5,22 @@ from model.state import State
 from AI.ai import H
 
 class HeuristicMenu(State):
-
-    def __init__(self, ai_algorithm = None):
+    """
+    Class that represents the model for the heuristics menu
+    """
+    def __init__(self, ai_algorithm=None) -> None:
+        """
+        Constructor for the Heuristics menu
+        :param ai_algorithm: Algorithm that the AI uses
+        :type ai_algorithm: enum AIS
+        :return: None
+        """
         super().__init__()
         self.ai_algorithm = ai_algorithm
 
     def _create_buttons(self) -> None:
         """
-        Creates the buttons for the hueristic menu
+        Creates the buttons for the heuristic menu
         :return: None
         """
         self.buttons = [
