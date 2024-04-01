e34acfcc17107ff8598977c86a58edc03b1b9b1c
mansur <mustafin.mansur02@gmail.com>
2024-04-01 19:51:12 +0100

    documentation service/controller/

--- src/service/controller/controller.py
+++ src/service/controller/controller.py
@@ -6,13 +6,32 @@ import pygame
 from view.view import View
 from settings import *
 
+
 class Controller(ABC):
+    """
+    An abstract base class representing a controller in a game architecture.
+    Attributes:
+    ----------
+        state: The current state of the game or application.
+        view: The view component associated with the game or application.
+    """
 
     def __init__(self, state, view):
+        """
+        Initializes the Controller with the given state and view.
+
+        :param state: The state of the game or application.
+        :param view: The view of the game or application.
+        """
         self.state = state
         self.view = view
 
     def handle_event(self) -> Optional[Command]:
+        """
+        Processes Pygame events and triggers corresponding actions.
+        This method handles the quit event, mouse motion, and button presses.
+        :return: Optional[Command]
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.WINDOWCLOSE:
                 return Command.EXIT
@@ -30,15 +49,33 @@ class Controller(ABC):
                     return self.handle_pressed_button(button)
 
     @abstractmethod
-    def handle_pressed_button(self, button):
+    def handle_pressed_button(self, button) -> Optional[Command]:
+        """
+        An abstract method to be implemented by subclasses to define actions when a button is pressed.
+        :param button: The button that was pressed.
+        :return: Optional[Command]
+        """
         pass
 
     def get_view(self) -> View:
+        """
+        Returns the current view of the controller.
+
+        :return: The current view.
+        """
         return self.view
 
     def get_state(self) -> any:
+        """
+        Returns the current state of the controller.
+
+        :return: The current state.
+        """
         return self.state
 
     def update_screen(self):
+        """
+        Draws the screen based on the current state and updates the Pygame display.
+        """
         self.view.draw_screen(self.state)
         pygame.display.update()
--- src/service/controller/gameController.py
+++ src/service/controller/gameController.py
@@ -1,32 +1,47 @@
-from typing import Optional
-from copy import deepcopy
 import time
+from copy import deepcopy
+from typing import Optional
+
+from AI.ai import AI, AIS, H
+from model.button import Button
 from model.endMenu import EndMenu
 from model.mainMenu import MainMenu
+from view.viewEndMenu import ViewEndMenu
 from view.viewMainMenu import ViewMainMenu
 from .controller import Controller, Command
-from model.button import Button
-from view.viewEndMenu import ViewEndMenu
-from AI.ai import AI, AIS, H
+
 
 class GameController(Controller):
-
+    """
+    A controller class for handling game logic, including responding to button presses, managing game state changes.
+    Inherits from Controller.
+    """
     def __init__(self, state, view):
+        """
+        Initializes the GameController with the specified game state and view.
+        :param state: The state of the game.
+        :param view: The view of the game.
+        """
         super().__init__(state, view)
         self.state.time = time.time()
 
     def handle_pressed_button(self, button: Button) -> Optional[Command]:
+        """
+        Handles actions triggered by button presses during gameplay.
+        :param button: The button that was pressed.
+        :return: Optional[Command]
+        """
         action = button.get_action()
 
         if action == "Quit":
             self.state = MainMenu()
             self.view = ViewMainMenu(self.view.get_screen())
             return Command.CHANGE_MAIN
-        
+
         if action == "Help":
             if self.state.selected_button is not None:  # Caso se ele clockou no Help mas ja tinha clickado anted 
                 return None
-            
+
             level_copy = deepcopy(self.state)
             ai = AI(level_copy, AIS.ASTARW, H.MANHATTAN_PATTERN, 1, 2000)
             move = ai.moves[0]
@@ -43,18 +58,22 @@ class GameController(Controller):
         return None
 
     def process_move(self, move: str) -> None:
-        dir = move.split()[0]
-        indx = int(move.split()[1])
+        """
+        Processes a move by updating the game state accordingly.
+        :param move: The move to be processed.
+        :return: None
+        """
+        direction = move.split()[0]
+        index = int(move.split()[1])
 
-        self.state.unselect_button()    # o bolinho deseparece
+        self.state.unselect_button()  # o bolinho deseparece
         self.state.increment_score()
 
-        if dir == "right":
-            self.state.move_row(indx, 1, False)
-        elif dir == "left":
-            self.state.move_row(indx, -1, False)
-        elif dir == "up":
-            self.state.move_col(indx, -1, False)
-        elif dir == "down":
-            self.state.move_col(indx, 1, False)
-
+        if direction == "right":
+            self.state.move_row(index, 1, False)
+        elif direction == "left":
+            self.state.move_row(index, -1, False)
+        elif direction == "up":
+            self.state.move_col(index, -1, False)
+        elif direction == "down":
+            self.state.move_col(index, 1, False)
