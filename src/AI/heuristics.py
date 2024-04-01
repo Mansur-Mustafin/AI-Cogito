c43dd36cb4f1da82af4f8bd7cdd3582b269f0492
dsferreira <unidsferreira2003@gmail.com>
2024-04-01 14:13:39 +0100

    Documented heuristics methods

--- src/AI/heuristics.py
+++ src/AI/heuristics.py
@@ -1,53 +1,64 @@
 from model.level import Level
 
 def miss_match_heuristic(level:Level):
+    """
+        :param level: state of the level to evaluate
+        :type level: Level
+        :return: The number of tiles that aren't in the correct place in regards to the target board
+        :rtype: int
+    """
     return level.count_mismatched_tiles()
 
-
-
 def row_collum_miss_match_heuristic(level: Level):
+    """
+        :param level: state of the level to evaluate
+        :type level: Level
+        :return: The sum of missmatched tiles of every row and column of the board
+        :rtype: int
+    """
     dim = level.get_dimension()
     res = 0
     for i in range( dim ):
-        curr_line = level.get_row_pieces( i)
-        target_line = level.get_row_pieces( i)
-        curr_col = level.get_col_pieces( i, False)
-        target_col = level.get_col_pieces( i, False)
+        curr_line = level.get_row_pieces(i)
+        target_line = level.get_row_pieces(i)
+        curr_col = level.get_col_pieces(i, False)
+        target_col = level.get_col_pieces(i, False)
         for color in level.get_main_colors():
             res += abs(target_line[color] - curr_line[color])
             res += abs(target_col[color] - curr_col[color])
     return res
 
-
-
 def manhattan_distance(level:Level):
+    """
+        :param level: state of the level to evaluate
+        :type level: Level
+        :return: The sum of the manhattan distances of every tile in the current board
+          to the closest valid tile in the target board
+        :rtype: int
+    """
     res = 0
     
     for cord1, color in level.current_block.items():
         min_dist = 2* level.dimension
         for cord2, color2 in level.target_pattern.items():
-            if( color == color2 ):#and not (cord2 in associations)):
+            if color == color2:
                 dist = level.manhattan_distance(cord1,cord2)
-                if ( min_dist > dist):
+                if min_dist > dist:
                     min_dist = dist
         res += min_dist
     return res
-    
-def manhattan_distance_with_pattern(level:Level):
-        res = 0
-        for cord1, color in level.current_block.items():
-            min_dist = 2* level.dimension
-            for cord2, color2 in level.target_pattern.items():
-                if( color == color2 ):#and not (cord2 in associations)):
-                    dist = level.manhattan_distance(cord1,cord2)
-                    if ( min_dist > dist):
-                        min_dist = dist
-            res += min_dist
-        res +=  pattern(level)
-
-        return res
 
 def pattern(level:Level):
+    """
+        A row (or column) in the correct pattern is a row that only needs to be shifted right/left
+        (up/down if it is a column) to be in the correct position.
+        :param level: state of the level to evaluate
+        :type level: Level
+        :return: The sum of the missmatch heuristic with the negative value of the number
+            of rows and columns that have the correct pattern in comparison to the target board.
+        :rtype: int
+    """
+
     count = 0
     for l in range(level.dimension):
         line_curr = ['y'] * level.dimension
@@ -72,7 +83,24 @@ def pattern(level:Level):
             count+=1
     return -count*5 + level.count_mismatched_tiles()
 
-    
+def manhattan_distance_with_pattern(level:Level):
+    """
+        This particular heuristic mixes the pattern and manhattan distance heuristics to 
+        achieve better results
+        :param level: state of the level to evaluate
+        :type level: Level
+        :return: The sum between the manhattan and pattern heuristics
+        :rtype: int
+    """
+    res = 0
+    for cord1, color in level.current_block.items():
+        min_dist = 2* level.dimension
+        for cord2, color2 in level.target_pattern.items():
+            if color == color2:
+                dist = level.manhattan_distance(cord1,cord2)
+                if min_dist > dist:
+                    min_dist = dist
+        res += min_dist
+    res += pattern(level)
 
-
-
+    return res
